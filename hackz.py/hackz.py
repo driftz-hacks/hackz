9a94e1504844c9e285e58ce88296e4f1e51667bc
driftz-hacks <67556229+driftz-hacks@users.noreply.github.com>
2020-07-02 16:48:26 -0400

    Add files via upload

---
+++ hackz.py/hackz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+print ("─────█─▄▀█──█▀▄─█─────")
+print ("────▐▌──────────▐▌────")
+print ("────█▌▀▄──▄▄──▄▀▐█────")
+print ("───▐██──▀▀──▀▀──██▌───")
+print ("──▄████▄──▐▌──▄████▄──")
+print ("welcome to hackz")
+
+print ("-------------------------------this script is just made for beginners to kali and to look good---------------------")
+print ("-------------------------------lol---------------------------")
+print ("=================== if you want to git clone something you will need to run the command (git clone (the git link wich could be found on the git page there will be a green icon that says code then you will want to click it then the icon that has a clipboard)====================================")
+print ("for most scripts you will have to cd (change directory) into them. why? otherwise you will type the command in to run it but it will say (no such file or directory) but make sure before that you will have to cd into the folder it was downloaded in e.g cd Desktop. if you downloaded it into your desktop.")
+
+
+print ("how do i root? it is easy. all you have to do is type. sudo su")
+print ("then it will ask you to enter your kali password but you wont be able to see it")
+
+print ("now for the real part")
+
+nickname_name = input("please type in your nickname. outside world or virtual world: ")
+print("the name you entered is: ", nickname_name)
+type(nickname_name)
+
+print ("thank you i hope you enjoy using this script")
+
+print ("choose what you want to learn type 1 for nmap")
+
+print ("thank you for choosing")
+print (" this script is still new so there is not many features yet")
+print ("nmap is a simple tool where you can check ip's for vunrebilities")
+
+
+print ("this is easy to the git clone version just simply go onto my account in github or type in the search bar. nmapscanner-v1 and you will find it. other people do scanners")
